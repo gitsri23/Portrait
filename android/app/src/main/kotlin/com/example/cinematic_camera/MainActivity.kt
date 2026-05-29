0cd27232467ae4a439215d819c6c16501b9ec2df
gitsri23 <59004304+gitsri23@users.noreply.github.com>
2026-05-29 13:08:19 +0530

    Delete android/app directory

--- android/app/src/main/kotlin/com/example/cinematic_camera/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cinematic_camera
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    // వీడియో పికింగ్ మరియు ప్లేయర్ లాజిక్ అంతా ఫ్లట్టర్ ప్లగిన్స్ ద్వారా 
-    // సేఫ్ గా హ్యాండిల్ అవుతుంది కాబట్టి ఇక్కడ క్రాష్ అయ్యే హెవీ కోడ్ ఏమీ ఉండదు.
-}
